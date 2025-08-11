e59764f00b922a0629e143807744d53e789c41ba
matheus biesek <md.biesek@gmail.com>
2025-08-11 18:06:05 -0300

    feat(spring-todo): Inicializa projeto Spring Boot com dependências básicas. - Spring Web - Spring Data JPA - Spring Validation - Lombok - PostgreSQL

---
+++ spring-todo/src/main/java/com/matheusbiesek/todolist/spring_todo/SpringTodoApplication.java
@@ -0,0 +1,13 @@
+package com.matheusbiesek.todolist.spring_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoApplication.class, args);
+	}
+
+}
